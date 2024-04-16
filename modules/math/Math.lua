4745d1df85da4a7d789de7282c0419b0416d7c94
Miku AuahDark <lumi.hotaru@gmail.com>
2024-04-16 14:55:03 +0800

    Remove love.math.compress and love.math.decompress.

--- modules/math/Math.lua
+++ modules/math/Math.lua
@@ -118,132 +118,6 @@ return {
             },
         },
         {
-            name = 'compress',
-            description = 'Compresses a string or data using a specific compression algorithm.',
-            variants = {
-                {
-                    arguments = {
-                        {
-                            type = 'string',
-                            name = 'rawstring',
-                            description = 'The raw (un-compressed) string to compress.',
-                        },
-                        {
-                            type = 'CompressedDataFormat',
-                            name = 'format',
-                            description = 'The format to use when compressing the string.',
-                            default = '\'lz4\'',
-                        },
-                        {
-                            type = 'number',
-                            name = 'level',
-                            description = 'The level of compression to use, between 0 and 9. -1 indicates the default level. The meaning of this argument depends on the compression format being used.',
-                            default = '-1',
-                        },
-                    },
-                    returns = {
-                        {
-                            type = 'CompressedData',
-                            name = 'compressedData',
-                            description = 'A new Data object containing the compressed version of the string.',
-                        },
-                    },
-                },
-                {
-                    arguments = {
-                        {
-                            type = 'Data',
-                            name = 'data',
-                            description = 'A Data object containing the raw (un-compressed) data to compress.',
-                        },
-                        {
-                            type = 'CompressedDataFormat',
-                            name = 'format',
-                            description = 'The format to use when compressing the data.',
-                            default = '\'lz4\'',
-                        },
-                        {
-                            type = 'number',
-                            name = 'level',
-                            description = 'The level of compression to use, between 0 and 9. -1 indicates the default level. The meaning of this argument depends on the compression format being used.',
-                            default = '-1',
-                        },
-                    },
-                    returns = {
-                        {
-                            type = 'CompressedData',
-                            name = 'compressedData',
-                            description = 'A new Data object containing the compressed version of the raw data.',
-                        },
-                    },
-                },
-            },
-        },
-        {
-            name = 'decompress',
-            description = 'Decompresses a CompressedData or previously compressed string or Data object.',
-            variants = {
-                {
-                    arguments = {
-                        {
-                            type = 'CompressedData',
-                            name = 'compressedData',
-                            description = 'The compressed data to decompress.',
-                        },
-                    },
-                    returns = {
-                        {
-                            type = 'string',
-                            name = 'rawstring',
-                            description = 'A string containing the raw decompressed data.',
-                        },
-                    },
-                },
-                {
-                    arguments = {
-                        {
-                            type = 'string',
-                            name = 'compressedstring',
-                            description = 'A string containing data previously compressed with love.math.compress.',
-                        },
-                        {
-                            type = 'CompressedDataFormat',
-                            name = 'format',
-                            description = 'The format that was used to compress the given string.',
-                        },
-                    },
-                    returns = {
-                        {
-                            type = 'string',
-                            name = 'rawstring',
-                            description = 'A string containing the raw decompressed data.',
-                        },
-                    },
-                },
-                {
-                    arguments = {
-                        {
-                            type = 'Data',
-                            name = 'data',
-                            description = 'A Data object containing data previously compressed with love.math.compress.',
-                        },
-                        {
-                            type = 'CompressedDataFormat',
-                            name = 'format',
-                            description = 'The format that was used to compress the given data.',
-                        },
-                    },
-                    returns = {
-                        {
-                            type = 'string',
-                            name = 'rawstring',
-                            description = 'A string containing the raw decompressed data.',
-                        },
-                    },
-                },
-            },
-        },
-        {
             name = 'gammaToLinear',
             description = 'Converts a color from gamma-space (sRGB) to linear-space (RGB). This is useful when doing gamma-correct rendering and you need to do math in linear RGB in the few cases where LÖVE doesn\'t handle conversions automatically.\n\nRead more about gamma-correct rendering here, here, and here.\n\nIn versions prior to 11.0, color component values were within the range of 0 to 255 instead of 0 to 1.',
             variants = {
@@ -704,6 +578,111 @@ return {
                             type = 'Transform',
                             name = 'transform',
                             description = 'The new Transform object.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'noise',
+            description = 'Generates a Simplex or Perlin noise value in 1-4 dimensions. The return value will always be the same, given the same arguments.\n\nSimplex noise is closely related to Perlin noise. It is widely used for procedural content generation.\n\nThere are many webpages which discuss Perlin and Simplex noise in detail.',
+            -- deprecated = 'replaced by love.math.perlinNoise and love.math.simplexNoise',
+            variants = {
+                {
+                    description = 'Generates Simplex noise from 1 dimension.',
+                    arguments = {
+                        {
+                            type = 'number',
+                            name = 'x',
+                            description = 'The number used to generate the noise value.',
+                        },
+                    },
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'value',
+                            description = 'The noise value in the range of 1.',
+                        },
+                    },
+                },
+                {
+                    description = 'Generates Simplex noise from 2 dimensions.',
+                    arguments = {
+                        {
+                            type = 'number',
+                            name = 'x',
+                            description = 'The first value of the 2-dimensional vector used to generate the noise value.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'y',
+                            description = 'The second value of the 2-dimensional vector used to generate the noise value.',
+                        },
+                    },
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'value',
+                            description = 'The noise value in the range of 1.',
+                        },
+                    },
+                },
+                {
+                    description = 'Generates Perlin noise (Simplex noise in version 0.9.2 and older) from 3 dimensions.',
+                    arguments = {
+                        {
+                            type = 'number',
+                            name = 'x',
+                            description = 'The first value of the 3-dimensional vector used to generate the noise value.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'y',
+                            description = 'The second value of the 3-dimensional vector used to generate the noise value.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'z',
+                            description = 'The third value of the 3-dimensional vector used to generate the noise value.',
+                        },
+                    },
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'value',
+                            description = 'The noise value in the range of 1.',
+                        },
+                    },
+                },
+                {
+                    description = 'Generates Perlin noise (Simplex noise in version 0.9.2 and older) from 4 dimensions.',
+                    arguments = {
+                        {
+                            type = 'number',
+                            name = 'x',
+                            description = 'The first value of the 4-dimensional vector used to generate the noise value.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'y',
+                            description = 'The second value of the 4-dimensional vector used to generate the noise value.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'z',
+                            description = 'The third value of the 4-dimensional vector used to generate the noise value.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'w',
+                            description = 'The fourth value of the 4-dimensional vector used to generate the noise value.',
+                        },
+                    },
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'value',
+                            description = 'The noise value in the range of 1.',
                         },
                     },
                 },
